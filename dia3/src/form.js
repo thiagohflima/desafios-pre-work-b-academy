b8d442c730369aeb4b7389d04db45aa3a00aaaea
Thiago Lima <thiagohflima@gmail.com>
2021-08-21 03:14:06 -0300

    Dia3: Exercise 02

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,4 +1,4 @@
-const input = document.querySelector('[ data-js = "input_name"]')
+const input = document.querySelector('[data-js = "input_name"]')
 
 const exceptions = ['da', 'de', 'do', 'dos']
 
@@ -13,4 +13,36 @@ input.addEventListener('input', (e) => {
       return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
 
   }).join(' ')
+})
+
+const form = document.querySelector('[data-js = "form"]')
+const select = document.createElement('select')
+const colors = ['red', 'blue', 'yellow', 'green', 'black']
+
+select.setAttribute('multiple', '')
+form.appendChild(select)
+
+colors.forEach(color => {
+  const option = document.createElement('option')
+  option.value = option.textContent = color
+  select.appendChild(option)
+})
+
+const div = document.createElement('div')
+div.style.display = 'flex'
+form.appendChild(div)
+
+function createSquare(color) {
+  const squareColorized = document.createElement('div')
+  squareColorized.style.width = '100px'
+  squareColorized.style.height = '100px'
+  squareColorized.style.background = color
+  div.appendChild(squareColorized)
+}
+
+select.addEventListener('change', (e) => {
+  div.innerHTML = ''
+  Array.from(e.target.selectedOptions).map(option => {
+    createSquare(option.value)
+  })
 })
